27133bb3d73912a4978bf5495129d4db2d48f55d
Niccolò Musso Barcucci <niccolo.mussobarcucci@gmail.com>
2018-01-03 16:34:28 +0100

    added graphics and colors

--- wish.js
+++ wish.js
@@ -3,13 +3,14 @@ var isDragging = false;
 var isPressed = false;
 var clickMousePosition=[0,0];
 
+var connections=$('connection, inner');
 
 
 $( document ).ready(function() {
 	 
 	draw();
-	$("#root").css('left', 200);
-	$("#root").css('top', 200);
+	$("#root").css('left', 400);
+	$("#root").css('top', 150);
 	function draw() {
 	  // call again next time we can draw
 	  requestAnimationFrame(draw);
@@ -32,6 +33,7 @@ $( document ).ready(function() {
 	    if (isDragging){
 	    	$("#root").css('left', event.pageX-clickMousePosition[0]);
 	    	$("#root").css('top', event.pageY-clickMousePosition[1]);
+	    	connections.connections('update');
 	    }
 	 })
 	.mouseup(function() {
@@ -51,10 +53,55 @@ $( document ).ready(function() {
 });
 
 
-function calculatePositions(){
-	to be implemented
+function calculateSubPositions(object, min_angle, max_angle){
+	var nodes=object.children(".divnode");
+	var number_of_nodes=nodes.length;
+	if (number_of_nodes>0){
+		var angle_increment=(max_angle-min_angle)/(number_of_nodes+1);
+
+		nodes.each(function(i,obj){
+			var angle=min_angle+(angle_increment*(i+1));
+			var left=Math.sin(angle*Math.PI/180)*100;
+			var top=Math.cos(angle*Math.PI/180)*100;
+			var color=angleToColor(angle/360);
+			$(obj).css('left',left);
+			$(obj).css('top',top);
+			$(obj).children(".node").css('border-color',color);
+			addColoredLine(object.children('.node'),$(obj).children(".node"),color);
+			calculateSubPositions($(obj),angle,angle+angle_increment);
+		});
+	}
 }
 
+
+function angleToColor(h,s,l){
+
+    var hue2rgb = function hue2rgb(t){
+        if(t < 0) t += 1;
+        if(t > 1) t -= 1;
+        if(t < 1/6) return 6 * t;
+        if(t < 1/2) return 1;
+        if(t < 2/3) return 6 * (2/3 - t);
+        return 0;
+    }
+
+    var r = hue2rgb(h + 1/3);
+    var g = hue2rgb(h);
+    var b = hue2rgb(h - 1/3);
+
+
+     return 'rgb('+[Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)].join(', ')+')';
+}
+
+
+
+
+function addColoredLine(parent,child,color){
+	parent.connections({to: child, css:{color:color}});
+	connections = $('connection, inner');
+	connections.connections('update');
+}
+   
 
 
 function clickmanagement(event) {
@@ -67,13 +114,15 @@ function clickmanagement(event) {
 
                 result = prompt("New Node","new content");
                 if (result!="" && result!=null){
-                	object.append('<div><p class="node">'+result+'</p></div>');
+                	object.append('<div class="divnode"><p class="node">'+result+'</p></div>');
                 	$(".node")
                 	.unbind() //to prevent multiple listeners on already created divs
 					.mouseup(clickmanagement)
 					.dblclick(function(event){
 						event.preventDefault();  //cancel system double-click event
 					});
+
+					calculateSubPositions($("#root"),0,360);
                 }
                 clicks = 0;             //after action performed, reset counter
             }, DELAY,$(this).parent());
